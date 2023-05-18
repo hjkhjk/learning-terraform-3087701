565b8cdbbbb782eb91be1c797a7a16acd146429b
hjkhjk <hjkhjk@users.noreply.github.com>
2023-05-18 16:18:27 +1000

    fixed duplicate resource

--- main.tf
+++ main.tf
@@ -14,17 +14,6 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
-  
-  vpc_security_group_ids = [aws_security_group.blog.id]
-  
-  tags = {
-    Name = "HelloWorld"
-  }
-}
-
 data "aws_vpc" "default" {
   default = true
 }
