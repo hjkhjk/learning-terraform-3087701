6f1f699fb67d116567bf52c0c5db272679b4938d
hjkhjk <hjkhjk@users.noreply.github.com>
2023-05-19 15:04:19 +1000

    Add ALB - 03

--- main.tf
+++ main.tf
@@ -86,7 +86,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
   name = "blog_new"
-  vpc_id = module.vpc.vpc_id
+  vpc_id = "module.vpc.vpc_id"
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
