0d4b378218fe0e13a3691b8d5ea7c2bd96fae835
hjkhjk <hjkhjk@users.noreply.github.com>
2023-05-19 15:50:35 +1000

    Add autoscaling - 01

--- main.tf
+++ main.tf
@@ -30,15 +30,20 @@ module "vpc" {
   }
 }
 
-resource "aws_instance" "blog" {
-  ami                    = data.aws_ami.app_ami.id
-  instance_type          = var.instance_type
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
-  subnet_id = module.vpc.public_subnets[0]
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "6.10.0"
+  
+  name = "blog_as"
+  min_size = 1
+  max_size = 2
 
-  tags = {
-    Name = "Learning Terraform"
-  }
+  vpc_zone_identifier = module.vpc.public_subnets
+  target_group_arns   = module.alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id               = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
 }
 
 module "alb" {
