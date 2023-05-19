21320fb85311a3fd06200ee403c1e8b7cbbf9345
hjkhjk <hjkhjk@users.noreply.github.com>
2023-05-19 15:20:54 +1000

    Add ALB - 04

--- main.tf
+++ main.tf
@@ -49,7 +49,7 @@ module "alb" {
 
   load_balancer_type = "application"
 
-  vpc_id             = "module.vpc.vpc_id"
+  vpc_id             = module.vpc.vpc_id
   subnets            = ["module.vpc.public_subnets"]
   security_groups    = ["module.blog_sg.security_group_id"]
 
@@ -86,7 +86,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.2"
   name = "blog_new"
-  vpc_id = "module.vpc.vpc_id"
+  vpc_id = module.vpc.vpc_id
 
   ingress_rules = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
